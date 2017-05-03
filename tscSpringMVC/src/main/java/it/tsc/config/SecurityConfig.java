00ad0f5a7c73409e859075a64a3caa53a41727b9
astraservice <astraservice@astraservice-X200MA>
2017-05-03 19:14:27 +0200

    last update

--- tscSpringMVC/src/main/java/it/tsc/config/SecurityConfig.java
+++ tscSpringMVC/src/main/java/it/tsc/config/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -24,12 +26,38 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	  @Autowired
 	  public void configureGlobal(AuthenticationManagerBuilder auth){
 	    try {
-			auth
-			  .inMemoryAuthentication()
-			    .withUser("user").password("password").roles("USER");
+	    	auth
+	        .inMemoryAuthentication()
+	          .withUser("user")  // #1
+	            .password("password")
+	            .roles("USER")
+	            .and()
+	          .withUser("admin") // #2
+	            .password("password")
+	            .roles("ADMIN","USER");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			logger.error(e.getMessage());
 		}
+	  }
+	  
+	  @Override
+	  public void configure(WebSecurity web) throws Exception {
+	    web
+	      .ignoring()
+	         .antMatchers("/resources/**"); // #3
+	  }
+	  
+	  @Override
+	  protected void configure(HttpSecurity http) throws Exception {
+	    http.
+	    authorizeRequests()
+	    .antMatchers("/admin/**").hasRole("ADMIN")
+	    .antMatchers("/user/**").hasRole("USER")
+	    .anyRequest()
+	    .authenticated()
+	    .and()
+	    . authorizeRequests()
+	    .antMatchers("/login","/logout").permitAll(); // #4
 	  }
 }
--- tscSpringMVC/src/main/java/it/tsc/controller/LoginController.java
+++ tscSpringMVC/src/main/java/it/tsc/controller/LoginController.java
@@ -4,11 +4,34 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
 
     private static Logger logger = LoggerFactory.getLogger(LoginController.class);
+    
+    @RequestMapping(value = "/login", method = RequestMethod.HEAD)
+    public void login(){
+
+        logger.trace("trace logging");
+        logger.debug("debug logging");
+        logger.info("info logging");
+        logger.warn("warning logging");
+        logger.error("error logging", new RuntimeException("help"));
+
+    }
+    
+    @RequestMapping(value = "/logout", method = RequestMethod.HEAD)
+    public void logout(){
+
+        logger.trace("trace logging");
+        logger.debug("debug logging");
+        logger.info("info logging");
+        logger.warn("warning logging");
+        logger.error("error logging", new RuntimeException("help"));
+
+    }
     
 }
