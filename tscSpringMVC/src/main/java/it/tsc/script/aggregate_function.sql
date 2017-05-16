12303b59bdb117ea439f778dc32cf557713636de
astraservice <astraservice@astraservice-X200MA>
2017-05-16 18:38:36 +0200

    test aggregate,function

--- tscSpringMVC/src/main/java/it/tsc/script/aggregate_function.sql
+++ tscSpringMVC/src/main/java/it/tsc/script/aggregate_function.sql
@@ -1,17 +1,15 @@
-CREATE OR REPLACE FUNCTION ks_tsc.create_group_from_element( elements map<text, text>, element_a text, element_b text )
+CREATE OR REPLACE FUNCTION ks_tsc.create_group_from_element( elements set<text>, element_a text, element_b text )
 CALLED ON NULL INPUT
-RETURNS map<text, text>
+RETURNS set<text>
 LANGUAGE java AS '
-String val = (String) elements.get(element_a);
-if (val == null) elements.put(element_a, element_b); 
-else if (!elements.containsKey(element_a)) elements.put(element_a, element_b);
-return elements; ' ;
+elements.add(element_a);
+return elements; ';
 
 DROP FUNCTION IF EXISTS ks_tsc.create_group_from_element;
 
 CREATE OR REPLACE AGGREGATE ks_tsc.group_by_element(text, text) 
 SFUNC create_group_from_element 
-STYPE map<text, text> 
+STYPE set<text>
 INITCOND {};
 
 DROP AGGREGATE IF EXISTS ks_tsc.group_by_element;
