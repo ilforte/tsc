425df4f648ecbf9af18e4ffc63717c4e193d650d
ivan.melzani@beta80group.it <ivan.melzani@beta80group.it>
2017-10-12 09:35:41 +0200

    add eclipse link

---
+++ tscSpringMVC/src/main/java/it/tsc/entity/GroupEntity.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package it.tsc.entity;
+
+import javax.persistence.Table;
+
+/**
+ * @author astraservice
+ *
+ */
+@Table(name = "group", schema = "ks_tsc@cassandra_pu")
+public class GroupEntity {
+
+}
