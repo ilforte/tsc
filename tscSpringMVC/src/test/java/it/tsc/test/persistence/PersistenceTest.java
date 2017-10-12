fad46a3e31dc6fdb9eedf78aca96e00e83dab738
ivan.melzani@beta80group.it <ivan.melzani@beta80group.it>
2017-10-12 09:44:29 +0200

    persistence Test class

---
+++ tscSpringMVC/src/test/java/it/tsc/test/persistence/PersistenceTest.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package it.tsc.test.persistence;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.Test;
+
+import com.impetus.client.cassandra.common.CassandraConstants;
+
+/**
+ * @author astraservice
+ *
+ */
+public class PersistenceTest {
+  
+  @Test
+  public void testPersistence() {
+    //enable CQL3
+    Map<String, String> props = new HashMap<>();
+    props.put(CassandraConstants.CQL_VERSION, CassandraConstants.CQL_VERSION_3_0);
+    props.put("kundera.nodes","localhost");
+    props.put("kundera.port","9042");
+    props.put("kundera.username","cassandradbuser");
+    props.put("kundera.password","v7^bEWnuHaFXtXfv");
+    props.put("kundera.keyspace","ks_tsc");
+    props.put("kundera.dialect","cassandra");
+    props.put("kundera.client.lookup.class","com.impetus.kundera.client.cassandra.dsdriver.DSClientFactory");
+    
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("cassandra_pu", props);
+    
+    EntityManager em = emf.createEntityManager();
+  }
+
+}
