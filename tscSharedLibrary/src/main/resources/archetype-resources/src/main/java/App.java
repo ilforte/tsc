1e1ece5b9e55a2374e0e7644b074316034601385
IvanMelaz <ivan.melzani@beta80group.it>
2017-10-21 12:38:14 +0200

    add shared jar with all libraries

---
+++ tscSharedLibrary/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $tscSpringMVC.tscSharedLibrary;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
