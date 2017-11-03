b6fa0e7f833a42fbe930c178b3ef01cef27fb699
IvanMelaz <ivan.melzani@beta80group.it>
2017-11-03 20:15:40 +0100

    add test

--- tscSpringMVC/src/test/java/it/tsc/test/ws/WSDialogTest.java
+++ tscSpringMVC/src/test/java/it/tsc/test/ws/WSDialogTest.java
@@ -5,7 +5,20 @@ package it.tsc.test.ws;
 
 import static org.junit.Assert.assertTrue;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.xml.namespace.QName;
+import javax.xml.rpc.ServiceException;
+import javax.xml.soap.SOAPException;
+import javax.xml.ws.BindingProvider;
 import javax.xml.ws.Endpoint;
+import javax.xml.ws.Service;
+import javax.xml.ws.handler.MessageContext;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -32,6 +45,8 @@ import it.tsc.webservice.TscWebService;
 public class WSDialogTest {
   private static Logger logger = LoggerFactory.getLogger(WSDialogTest.class);
   public static final String URI = "http://localhost:8080/tscSpringMVC/services/TscWebService";
+  private static final String WS_URL =
+      "http://localhost:8080/tscSpringMVC/services/TscWebService?wsdl";
 
   /**
    * 
@@ -51,6 +66,27 @@ public class WSDialogTest {
     boolean status = endpoint.isPublished();
     System.out.println("Web service status = " + status);
     assertTrue(status);
+  }
+
+  @Test
+  public void testSoapHeaders() throws SOAPException, ServiceException, MalformedURLException {
+    URL url = new URL(WS_URL);
+    QName qname = new QName("http://webservice.tsc.it/", "TscWebService");
+
+    Service service = Service.create(url, qname);
+    TscWebService tscWebService = service.getPort(TscWebService.class);
+
+    /******************* UserName & Password ******************************/
+    Map<String, Object> req_ctx = ((BindingProvider) tscWebService).getRequestContext();
+    req_ctx.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, WS_URL);
+
+    Map<String, List<String>> headers = new HashMap<String, List<String>>();
+    headers.put("Username", Collections.singletonList("mkyong"));
+    headers.put("Password", Collections.singletonList("password"));
+    req_ctx.put(MessageContext.HTTP_REQUEST_HEADERS, headers);
+    /**********************************************************************/
+
+    System.out.println(tscWebService.insertAllarmEuropeAssistance(null, null, null, null));
   }
 
 }
