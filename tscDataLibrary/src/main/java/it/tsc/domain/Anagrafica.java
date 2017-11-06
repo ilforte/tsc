22011d28ee724f7c77bb1a74c8545d71af09176b
IvanMelaz <ivan.melzani@beta80group.it>
2017-11-06 23:13:42 +0100

    @OneToMany example

--- tscDataLibrary/src/main/java/it/tsc/domain/Anagrafica.java
+++ tscDataLibrary/src/main/java/it/tsc/domain/Anagrafica.java
@@ -18,40 +18,60 @@ import com.google.gson.annotations.Expose;
 @Entity
 @Table(name = "tb_anagrafica", schema = "ks_tsc@cassandra_pu")
 @NamedQueries(value = {
-        @NamedQuery(name = Anagrafica.SELECT_ALL_ANAGRAFICA, 
-        		query = "SELECT a FROM Anagrafica a WHERE a.ab_codi = :ab_codi")
-})
+		@NamedQuery(name = Anagrafica.SELECT_ALL_ANAGRAFICA, query = "SELECT a FROM Anagrafica a WHERE a.ab_codi = :ab_codi") })
 public class Anagrafica {
-  public static final String SELECT_ALL_ANAGRAFICA = "json.select.anagrafica";
-  @Id
-  @Column
-  @Expose
-  private String ab_codi;
-  @Column
-  @Expose
-  private String nominativo;
+	public static final String SELECT_ALL_ANAGRAFICA = "json.select.anagrafica";
+	@Id
+	@Column
+	@Expose
+	private String ab_codi;
+	@Column
+	@Expose
+	private String nominativo;
+	@Column
+	@Expose
+	private Integer altezza;
+	@Column
+	@Expose
+	private Double peso;
 
-  /**
-   * 
-   */
-  public Anagrafica() {
+	/**
+	 * 
+	 */
+	public Anagrafica() {
 
-  }
+	}
 
-  public String getAb_codi() {
-    return ab_codi;
-  }
+	public Integer getAltezza() {
+		return altezza;
+	}
 
-  public void setAb_codi(String ab_codi) {
-    this.ab_codi = ab_codi;
-  }
+	public void setAltezza(Integer altezza) {
+		this.altezza = altezza;
+	}
 
-  public String getNominativo() {
-    return nominativo;
-  }
+	public Double getPeso() {
+		return peso;
+	}
 
-  public void setNominativo(String nominativo) {
-    this.nominativo = nominativo;
-  }
+	public void setPeso(Double peso) {
+		this.peso = peso;
+	}
+
+	public String getAb_codi() {
+		return ab_codi;
+	}
+
+	public void setAb_codi(String ab_codi) {
+		this.ab_codi = ab_codi;
+	}
+
+	public String getNominativo() {
+		return nominativo;
+	}
+
+	public void setNominativo(String nominativo) {
+		this.nominativo = nominativo;
+	}
 
 }
--- tscDataLibrary/src/main/java/it/tsc/domain/Users.java
+++ tscDataLibrary/src/main/java/it/tsc/domain/Users.java
@@ -3,11 +3,17 @@
  */
 package it.tsc.domain;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.google.gson.annotations.Expose;
@@ -15,25 +21,17 @@ import com.google.gson.annotations.Expose;
 import it.tsc.domain.key.CompoundKey;
 
 /**
- * @author astraservice
- * tb_users
+ * @author astraservice tb_users
  */
 @Entity
 @Table(name = "tb_users", schema = "ks_tsc@cassandra_pu")
-@NamedQueries(value = {
-        @NamedQuery(name = Users.SELECT_ALL_USERS, 
-        		query = "SELECT u FROM Users u"),
-        @NamedQuery(name = Users.SELECT_BY_USERNAME, 
-        		query = "SELECT u FROM Users u WHERE u.key.username = :username"),
-        @NamedQuery(name = Users.SELECT_BY_USERNAME_EMAIL, 
-		query = "SELECT u FROM Users u WHERE u.key.username = :username AND u.email=:email"),
-        @NamedQuery(name = Users.UPDATE_BY_USERNAME_ROLE, 
-		query = "UPDATE Users u SET u.keyId=:keyId,u.base32Secret=:base32Secret WHERE u.key.username = :username AND u.key.role=:role"),
-        @NamedQuery(name = Users.UPDATE_USER, 
-		query = "UPDATE Users u SET u.password=:password,u.email=:email WHERE u.key.username = :username AND u.key.role = :role")
-})
-public class Users extends BaseDomain{
-    /**
+@NamedQueries(value = { @NamedQuery(name = Users.SELECT_ALL_USERS, query = "SELECT u FROM Users u"),
+		@NamedQuery(name = Users.SELECT_BY_USERNAME, query = "SELECT u FROM Users u WHERE u.key.username = :username"),
+		@NamedQuery(name = Users.SELECT_BY_USERNAME_EMAIL, query = "SELECT u FROM Users u WHERE u.key.username = :username AND u.email=:email"),
+		@NamedQuery(name = Users.UPDATE_BY_USERNAME_ROLE, query = "UPDATE Users u SET u.keyId=:keyId,u.base32Secret=:base32Secret WHERE u.key.username = :username AND u.key.role=:role"),
+		@NamedQuery(name = Users.UPDATE_USER, query = "UPDATE Users u SET u.password=:password,u.email=:email WHERE u.key.username = :username AND u.key.role = :role") })
+public class Users extends BaseDomain {
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -5293616948185366680L;
@@ -42,25 +40,42 @@ public class Users extends BaseDomain{
 	public static final String SELECT_BY_USERNAME_EMAIL = "select.by.username.email";
 	public static final String UPDATE_BY_USERNAME_ROLE = "update.by.username.role";
 	public static final String UPDATE_USER = "update.user";
-	
+
 	@EmbeddedId
 	@Expose
 	private CompoundKey key;
 	@Column
 	private String password;
-	
-	@Column(name="base32secret")
+
+	@Column(name = "base32secret")
 	@Expose
 	private String base32Secret;
-	
+
+	@Column(name = "mfaEnabled")
+	@Expose
+	private boolean mfaEnabled;
+
 	@Column
 	@Expose
 	private String email;
-	
-	@Column(name="keyid")
+
+	@Column(name = "keyid")
 	@Expose
 	private String keyId;
-	
+
+	@OneToMany(cascade = { CascadeType.ALL }, fetch = FetchType.EAGER)
+	@JoinColumn(name = "groupName", referencedColumnName = "groupName")
+	@Expose
+	private List<Group> groups;
+
+	public List<Group> getGroups() {
+		return groups;
+	}
+
+	public void setGroups(List<Group> groups) {
+		this.groups = groups;
+	}
+
 	public String getBase32Secret() {
 		return base32Secret;
 	}
@@ -100,18 +115,26 @@ public class Users extends BaseDomain{
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
+	public boolean isMfaEnabled() {
+		return mfaEnabled;
+	}
+
+	public void setMfaEnabled(boolean mfaEnabled) {
+		this.mfaEnabled = mfaEnabled;
+	}
+
 	public void addUsername(String username) {
-		if (this.key ==null) {
+		if (this.key == null) {
 			this.key = new CompoundKey();
 			this.key.setUsername(username);
 		} else {
 			this.key.setUsername(username);
 		}
 	}
-	
+
 	public void addRole(Role role) {
-		if (this.key ==null) {
+		if (this.key == null) {
 			this.key = new CompoundKey();
 			this.key.setRole(role.toString());
 		} else {
